1c11b640c2af6977167ce36baf20e4e6be053007
ogungbile <52929020+ogungbile@users.noreply.github.com>
2024-02-26 12:30:36 +0100

    Update urls.py

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import UserRegisterView
 #from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
