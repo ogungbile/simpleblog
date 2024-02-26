f2fed7183eadeea080e4a4e7abd584f71af78670
ogungbile <52929020+ogungbile@users.noreply.github.com>
2024-02-26 12:28:53 +0100

    Update urls.py

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
